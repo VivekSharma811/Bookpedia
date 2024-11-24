88834c2302cbf2b9f3b591717155feeeb23b9091
Vivek Sharma <vivek.sharma@mpokket.com>
2024-11-24 14:09:27 +0530

    Book List UI added

--- composeApp/src/commonMain/kotlin/com/hypheno/bookpedia/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.hypheno.bookpedia
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
---
+++ composeApp/src/commonMain/kotlin/com/hypheno/bookpedia/book/presentation/book_list/BookListState.kt
@@ -0,0 +1,30 @@
+package com.hypheno.bookpedia.book.presentation.book_list
+
+import com.hypheno.bookpedia.book.domain.Book
+import com.hypheno.bookpedia.core.presentation.UiText
+
+
+data class BookListState(
+    val searchQuery: String = "Kotlin",
+    val searchResults: List<Book> = books,
+    val favoriteBooks: List<Book> = emptyList(),
+    val isLoading: Boolean = false,
+    val selectedTabIndex: Int = 0,
+    val errorMessage: UiText? = null
+)
+
+val books = (1..100).map {
+    Book(
+        id = it.toString(),
+        title = "Book $it",
+        imageUrl = "https://test.com",
+        authors = listOf("Philipp Lackner"),
+        description = "Description $it",
+        languages = emptyList(),
+        firstPublishYear = null,
+        averageRating = 4.67854,
+        ratingCount = 5,
+        numPages = 100,
+        numEditions = 3
+    )
+}
